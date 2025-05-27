ad2af1547420964a516c2f2c711f4cf5f5a0d6e0
Marcio Palma <marciop.itzamna@gmail.com>
2025-05-26 23:04:08 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,5 +30,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
