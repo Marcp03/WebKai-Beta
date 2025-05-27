04da8808b6aa9deff5451ff6b1813053dd930844
Marcio Palma <marciop.itzamna@gmail.com>
2025-05-26 23:12:38 -0600

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
-&nbsp;
-&nbsp;
+;
+;
 
 // For demo, we use a hardcoded admin user. In production, this would be in DB.
 const adminUser = {
@@ -12,8 +12,8 @@ const adminUser = {
   // password: 'kaiadmin2024' hashed:
   passwordHash: '$2b$10$Pb9X1o4ORM6TeQpSKtf2HOe6NLA5kDruOk3wo./i1xJ1NrN7Xv1DS'
 };
-&nbsp;
-&nbsp;
+;
+;
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
@@ -27,7 +27,7 @@ router.post('/login', async (req, res) => {
   const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '8h' });
   res.json({ token });
 });
-&nbsp;
-&nbsp;
+;
+;
 
 module.exports = router;
