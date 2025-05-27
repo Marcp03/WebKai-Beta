c673e8154a3019ce16a98d98f577995c396937b7
Marcio Palma <marciop.itzamna@gmail.com>
2025-05-26 23:07:24 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,30 +3,30 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const dataRoutes = require('./routes/data');
 require('dotenv').config();
-&nbsp;
-&nbsp;
+;
+;
 
 const app = express();
-&nbsp;
-&nbsp;
+;
+;
 
 app.use(cors());
 app.use(express.json());
-&nbsp;
-&nbsp;
+;
+;
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/data', dataRoutes);
-&nbsp;
-&nbsp;
+;
+;
 
 // Health check
 app.get('/', (req, res) => {
   res.send('Observatorio KAI+ backend funcionando');
 });
-&nbsp;
-&nbsp;
+;
+;
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
