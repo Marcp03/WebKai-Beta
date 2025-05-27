784fa604abd9ef69cf64cae83e735529eb4646de
Marcio Palma <marciop.itzamna@gmail.com>
2025-05-26 23:14:10 -0600

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,17 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-&nbsp;
-&nbsp;
+;
+;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
-&nbsp;
-&nbsp;
+;
+;
 pool.on('connect', () => {
   console.log('PostgreSQL connected');
 });
-&nbsp;
-&nbsp;
+;
+;
 module.exports = {
   query: (text, params) => pool.query(text, params),
   pool,
