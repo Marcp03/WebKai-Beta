bcb592d76e4717508f87b8776470b325c3790a89
Marcio Palma <marciop.itzamna@gmail.com>
2025-05-26 23:13:43 -0600

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,18 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-&nbsp;
-&nbsp;
+;
+;
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   if (!authHeader) return res.status(401).json({ message: 'Authorization header missing' });
-&nbsp;
-&nbsp;
+;
+;
 
   const token = authHeader.split(' ')[1];
   if (!token) return res.status(401).json({ message: 'Token missing' });
-&nbsp;
-&nbsp;
+;
+;
 
   jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
     if (err) return res.status(403).json({ message: 'Invalid or expired token' });
@@ -20,7 +20,7 @@ function authenticateToken(req, res, next) {
     next();
   });
 }
-&nbsp;
-&nbsp;
+;
+;
 
 module.exports = authenticateToken;
